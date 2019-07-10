c3ecd1449820da4d2a2773d213b9c05b245ea34a
Administrator <admin@example.com>
2019-07-10 15:20:10 +0800

    页面

--- jboot-admin-service/jboot-admin-service-provider/src/main/java/io/jboot/admin/service/provider/PtOrderServiceImpl.java
+++ jboot-admin-service/jboot-admin-service-provider/src/main/java/io/jboot/admin/service/provider/PtOrderServiceImpl.java
@@ -1,8 +1,12 @@
 package io.jboot.admin.service.provider;
 
+import com.jfinal.kit.StrKit;
+import com.jfinal.plugin.activerecord.Page;
+import io.jboot.admin.service.entity.model.Code;
 import io.jboot.aop.annotation.Bean;
 import io.jboot.admin.service.api.PtOrderService;
 import io.jboot.admin.service.entity.model.PtOrder;
+import io.jboot.db.model.Columns;
 import io.jboot.service.JbootServiceBase;
 
 import javax.inject.Singleton;
@@ -10,5 +14,14 @@ import javax.inject.Singleton;
 @Bean
 @Singleton
 public class PtOrderServiceImpl extends JbootServiceBase<PtOrder> implements PtOrderService {
+    @Override
+    public Page<PtOrder> findPage(PtOrder ptOrder, int pageNumber, int pageSize) {
 
+        Columns columns = Columns.create();
+
+        if (StrKit.notBlank(ptOrder.get("orderId")+"")) {
+            columns.like("user_name", "%"+ptOrder.get("orderId")+"%");
+        }
+        return DAO.paginateByColumns(pageNumber, pageSize, columns.getList(), "updated desc");
+    }
 }
